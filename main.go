ae5a9ad1c864b14029eda6149066dc3435805446
Jamal <Fox.Jamal@outlook.com>
2024-11-06 20:12:02 -0500

    simplify repo test

--- main.go
+++ main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"travel-app-api/api"
+	"fmt"
+	"net/http"
 
-	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// func helloWorld(w http.ResponseWriter, r *http.Request) {
-//     fmt.Fprintf(w, "Hello World")
-// }
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello World")
+}
 
 func main() {
     // Disable Extra Gin Logs
-    gin.SetMode(gin.ReleaseMode)
+    // gin.SetMode(gin.ReleaseMode)
 
     // Create app instance
-    app := api.NewApplication()
+    // app := api.NewApplication()
     
     // Set server port and start
-    listenAddr := "localhost:8080"
-    server := api.NewServer(listenAddr)
-    server.Start(app)
+    // listenAddr := "localhost:8080"
+    // server := api.NewServer(listenAddr)
+    // server.Start(app)
 
-    // http.HandleFunc("/", helloWorld)
-    // http.ListenAndServe(":5000", nil)
+    http.HandleFunc("/", helloWorld)
+    http.ListenAndServe(":5000", nil)
     
 }
