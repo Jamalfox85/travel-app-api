a1b2f5d50b50ed4370532821b444bb5fc44a7e3e
Jamal <Fox.Jamal@outlook.com>
2024-11-06 20:37:12 -0500

    route changes

--- main.go
+++ main.go
@@ -1,49 +1,30 @@
-// package main
-
-// import (
-// 	"travel-app-api/api"
-
-// 	"github.com/gin-gonic/gin"
-// 	_ "github.com/go-sql-driver/mysql"
-// )
-
-// // func helloWorld(w http.ResponseWriter, r *http.Request) {
-// //     fmt.Fprintf(w, "Hello World")
-// // }
-
-// func main() {
-//     // Disable Extra Gin Logs
-//     gin.SetMode(gin.ReleaseMode)
-
-//     // Create app instance
-//     app := api.NewApplication()
-
-//     // Set server port and start
-//     listenAddr := "localhost:8080"
-//     server := api.NewServer(listenAddr)
-//     server.Start(app)
-
-//     // http.HandleFunc("/", helloWorld)
-//     // http.ListenAndServe(":5000", nil)
-
-// }
-
 package main
 
 import (
 	"log"
-	"net/http"
 	"os"
+	"travel-app-api/api"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	router := gin.Default()
+    router := gin.Default()
 	router.Use(CORSMiddleware())
-	router.GET("/hello-world", myGetFunction)
+    // Disable Extra Gin Logs
+    gin.SetMode(gin.ReleaseMode)
 
-	port := os.Getenv("PORT")
+    // Create app instance
+    app := api.NewApplication()
+
+    // Set server port and start
+    listenAddr := "localhost:8080"
+    server := api.NewServer(listenAddr)
+    server.Start(app)
+
+
+    port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
@@ -68,16 +49,56 @@ func CORSMiddleware() gin.HandlerFunc {
 	}
 }
 
-type simpleMessage struct {
-	Hello           string `json:"hello"`
-	Message string `json:"message"`
-}
+// package main
 
-func myGetFunction(c *gin.Context) {
-	simpleMessage := simpleMessage{
-		Hello: "World!",
-		Message: "Subscribe to my channel!",
-	}
+// import (
+// 	"log"
+// 	"net/http"
+// 	"os"
 
-	c.IndentedJSON(http.StatusOK, simpleMessage)
-}
+// 	"github.com/gin-gonic/gin"
+// )
+
+// func main() {
+// 	router := gin.Default()
+// 	router.Use(CORSMiddleware())
+// 	router.GET("/hello-world", myGetFunction)
+
+// 	port := os.Getenv("PORT")
+// 	if port == "" {
+// 		port = "8080"
+// 	}
+// 	if err := router.Run(":" + port); err != nil {
+// 		log.Panicf("error: %s", err)
+// 	}
+// }
+
+// func CORSMiddleware() gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+// 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+// 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+// 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+// 		if c.Request.Method == "OPTIONS" {
+// 			c.AbortWithStatus(204)
+// 			return
+// 		}
+
+// 		c.Next()
+// 	}
+// }
+
+// type simpleMessage struct {
+// 	Hello           string `json:"hello"`
+// 	Message string `json:"message"`
+// }
+
+// func myGetFunction(c *gin.Context) {
+// 	simpleMessage := simpleMessage{
+// 		Hello: "World!",
+// 		Message: "Subscribe to my channel!",
+// 	}
+
+// 	c.IndentedJSON(http.StatusOK, simpleMessage)
+// }
