4b1b304c39aec274f43c3fc57cd4ac2129a492a4
Jamal <Fox.Jamal@outlook.com>
2024-11-08 17:13:56 -0500

    router test

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"os"
 	"travel-app-api/api"
 
@@ -10,8 +9,8 @@ import (
 )
 
 func main() {
-    router := gin.Default()
-	router.Use(CORSMiddleware())
+    // router := gin.Default()
+	// router.Use(CORSMiddleware())
     // Disable Extra Gin Logs
     gin.SetMode(gin.ReleaseMode)
 
@@ -28,9 +27,9 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
-	if err := router.Run(":" + port); err != nil {
-		log.Panicf("error: %s", err)
-	}
+	// if err := router.Run(":" + port); err != nil {
+	// 	log.Panicf("error: %s", err)
+	// }
 }
 
 func CORSMiddleware() gin.HandlerFunc {
@@ -48,57 +47,3 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Next()
 	}
 }
-
-// package main
-
-// import (
-// 	"log"
-// 	"net/http"
-// 	"os"
-
-// 	"github.com/gin-gonic/gin"
-// )
-
-// func main() {
-// 	router := gin.Default()
-// 	router.Use(CORSMiddleware())
-// 	router.GET("/hello-world", myGetFunction)
-
-// 	port := os.Getenv("PORT")
-// 	if port == "" {
-// 		port = "8080"
-// 	}
-// 	if err := router.Run(":" + port); err != nil {
-// 		log.Panicf("error: %s", err)
-// 	}
-// }
-
-// func CORSMiddleware() gin.HandlerFunc {
-// 	return func(c *gin.Context) {
-// 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-// 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-// 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-// 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-
-// 		if c.Request.Method == "OPTIONS" {
-// 			c.AbortWithStatus(204)
-// 			return
-// 		}
-
-// 		c.Next()
-// 	}
-// }
-
-// type simpleMessage struct {
-// 	Hello           string `json:"hello"`
-// 	Message string `json:"message"`
-// }
-
-// func myGetFunction(c *gin.Context) {
-// 	simpleMessage := simpleMessage{
-// 		Hello: "World!",
-// 		Message: "Subscribe to my channel!",
-// 	}
-
-// 	c.IndentedJSON(http.StatusOK, simpleMessage)
-// }
